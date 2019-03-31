d277e41adf42bab3976b575c082d65bc9cb91b66
zhang <1453512279@qq.com>
2019-04-01 01:39:10 +0800

    登录页面完成

---
+++ clientside/src/pages/Counsel/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Counsel extends Component {
+    render(){
+        return (
+            <div>Counsel</div>
+        );
+    }
+}
+
+
+export default Counsel;
